7f7e47f62bfc96a63fa0df6fdc40e7bd05fbddde
iiDuNoo <adam.dunn@users.noreply.github.com>
2020-02-23 21:41:54 -0500

    big commit
    processed data, ppt work, scripts to process data

--- preprocessing.py
+++
@@ -1,9 +0,0 @@
-#preprocessing dataset for Neural Network
-import csv
-
-for i in range (2013, 2020):
-    demanddata = open('PUB_DemandZonal_' + i + '.csv')
-    torontoweatherdata = open('toronto_weather_' + i + '.csv')
-    ottawaweatherdata = open('ottawa_weather_' + i + '.csv')
-    bruceweatherdata = open('bruce_weather_' + i + '.csv')
-    csvdata = csv.reader(demanddata)
---
+++ torontoweather_preprocessing.py
@@ -0,0 +1,36 @@
+#preprocessing dataset for Neural Network - only weather data
+import csv
+from datetime import datetime
+from pathlib import Path
+for i in range (2013, 2021): #repeats for data from 2013-2020
+    #load yearly data
+
+    #demanddata = open('C:\\Users\\Adam\\Documents\\GitHub\\Sustainable-AI-Challenge\\Raw_Dataset\\PUB_DemandZonal_'+ str(i) +'.csv','rt')
+    torontoweatherdata = open('C:\\Users\\Adam\\Documents\\GitHub\\Sustainable-AI-Challenge\\Raw_Dataset\\toronto_weather_' + str(i) + '.csv')
+    #ottawaweatherdata = open('C:\\Users\\Adam\\Documents\\GitHub\\Sustainable-AI-Challenge\\Raw_Dataset\\ottawa_weather_' + str(i) + '.csv')
+    #bruceweatherdata = open('C:\\Users\\Adam\\Documents\\GitHub\\Sustainable-AI-Challenge\\Raw_Dataset\\bruce_weather_' + str(i) + '.csv')
+    #csvdemand = csv.reader(demanddata,delimiter=',') #converts to csv
+    csvtoronto = csv.reader(torontoweatherdata,delimiter=',')
+    #csvottawa = csv.reader(ottawaweatherdata,delimiter=',')
+    #csvbruce = csv.reader(bruceweatherdata,delimiter=',')
+
+    with open('C:\\Users\\Adam\\Documents\\GitHub\\Sustainable-AI-Challenge\\Postprocessed_Dataset\\toronto_' + str(i) +'.csv', mode='w',newline='') as toronto_file:
+        toronto_writer = csv.writer(toronto_file, delimiter=',',quoting=csv.QUOTE_ALL)
+        toronto_writer.writerow(("Year","Month","Day","Hour","Temperature","Dew Point","Relative Humidity","Wind Speed",'\n')) #writes header
+        next(csvtoronto)
+        for row in csvtoronto:
+
+                year= (row[5])# finds data from csv row-column intercept
+                month =(row[6])
+                day= (row[7])
+                hour = (row[8])
+                h = datetime.strptime(hour, "%H:%M")
+                hour = h.hour
+                temp = (row[9])
+                dew = (row[11])
+                relhum = (row[13])
+                wspeed = (row[17])
+
+                toronto_writer.writerow((year, month, day,hour,temp,dew,relhum,wspeed)) #outputs to processed data
+
+
